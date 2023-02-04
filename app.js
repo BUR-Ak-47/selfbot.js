edb8a4300d2a43b9191f66de76118e576251cc51
Burak <124401463+BUR-Ak-47@users.noreply.github.com>
2023-02-04 16:05:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Selfbot made by Burak
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); // All partials are loaded automatically
 const { token } = require("./config.json");
@@ -8,13 +9,23 @@ client.on('ready', async () => {
 })
 
 client.on("message", message => {
-  if (message.content === "!Burak") 
+  if (message.content === "!burak") 
     message.channel.send("https://github.com/BUR-Ak-47")
 })
 
 client.on("message", message => {
-  if (message.content === "!Burak") 
-    message.channel.send("https://www.roblox.com/users/3808139090/profile")
+  if (message.content === "!burak") 
+    message.channel.send("https://www.youtube.com/channel/UCfYslLqChf9wVn94u9E70dg")
+})
+
+client.on("message", message => {
+  if (message.content === "!help") 
+    message.channel.send("help command")
+})
+
+client.on("message", message => {
+  if (message.content === "!template") 
+    message.channel.send("template command")
 })
 
 client.login(token);
