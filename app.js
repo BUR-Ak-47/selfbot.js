790efa1000f9cf5222f9d6dc43ecd74bcef5a826
Burak <124401463+BUR-Ak-47@users.noreply.github.com>
2023-02-04 15:57:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client(); // All partials are loaded automatically
+const { token } = require("./config.json");
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+  client.user.setActivity("github.com/BUR-Ak-47")
+})
+
+client.on("message", message => {
+  if (message.content === "!Burak") 
+    message.channel.send("https://github.com/BUR-Ak-47")
+})
+
+client.on("message", message => {
+  if (message.content === "!Burak") 
+    message.channel.send("https://www.roblox.com/users/3808139090/profile")
+})
+
+client.login(token);
